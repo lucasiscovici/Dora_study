808a1addfe77b65e7c9710563f24041bdebd4568
Your Name <you@example.com>
2019-11-27 23:34:27 +0100

    Dora-Mer 27 nov 2019 23:34:27 CET

--- Dora_study/main.py
+++ Dora_study/main.py
@@ -26,7 +26,7 @@ def saveLast_(self,func,*args,**kwargs):
   argss= inspect.getcallargs(func,self, *args, **kwargs)
   del argss["self"]
   argss=["{}={}".format(i,"\""+j+"\"" if isinstance(j,str) else j) for i,j in argss.items()]
-  argss={i:(inspect.getsource(j).strip()) if isinstance(j,types.LambdaType) else j) for i,j in argss.items()}
+  argss={i:(inspect.getsource(j).strip() if isinstance(j,types.LambdaType) else j) for i,j in argss.items()}
 
   self._log( "self.{}({})".format( func.__name__, ", ".join(argss) ) ,force=force)
   return rep
